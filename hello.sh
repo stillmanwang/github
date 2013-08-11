69507341bbc0bfcdef2d058c71a72019c1c84218
nwang <nwang@freewheel.tv>
2013-08-11 16:14:05 +0800

    new file:   hello.sh

---
+++ hello.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+######################################################################
+#
+# Author: Wang Ning - nwang@freewheel.tv
+# File: hello.sh
+# Desc:
+#
+# Log:
+#     Create by Wang Ning, 2013/08/11 15:08
+#     Last modified by Wang Ning, 2013/08/11 15:08
+#
+######################################################################
+
+echo "hello world!"
+exit 0
